6b4967d0754cc250af047fb461b9f68a080ef0d6
waeng <quanfenghuang@outlook.com>
2021-12-15 21:51:48 +0800

    - init

---
+++ src/main/java/waeng/bootrestapi/BootRestApiApplication.java
@@ -0,0 +1,13 @@
+package waeng.bootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/waeng/bootrestapi/BootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package waeng.bootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
